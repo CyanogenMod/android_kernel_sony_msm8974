67c3ac09271175b3da6569abd6cab8850772b0e6
Sree Sesha Aravind Vadrevu <svadrevu@codeaurora.org>
2013-07-23 12:44:39 -0700

    msm: mdss: Make wait_for_complete calls uninterruptible
    The wait_for_complete calls in the MDSS driver are associated
    with an acceptable timeout value which would remove the need for
    making them interruptible. Also, this prevents unwanted signal
    interrupts before the call is finished.
    
    Change-Id: If4013b0d74bc9f717c4a63d961f2a34753320e87
    Signed-off-by: Sree Sesha Aravind Vadrevu <svadrevu@codeaurora.org>

--- drivers/video/msm/mdss/mdss_mdp_intf_writeback.c
+++ drivers/video/msm/mdss/mdss_mdp_intf_writeback.c
@@ -344,9 +344,9 @@ static int mdss_mdp_wb_wait4comp(struct mdss_mdp_ctl *ctl, void *arg)
 	if (ctx->comp_cnt == 0)
 		return rc;
 
-	rc = wait_for_completion_interruptible_timeout(&ctx->wb_comp,
+	rc = wait_for_completion_timeout(&ctx->wb_comp,
 			KOFF_TIMEOUT);
-	if (rc <= 0) {
+	if (rc == 0) {
 		rc = -ENODEV;
 		WARN(1, "writeback kickoff timed out (%d) ctl=%d\n",
 						rc, ctl->num);
