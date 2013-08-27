ab75c7194b2a9e64c9cd346c7a22dfcfb5918056
Giulio Cervera <giulio.cervera@cyanogenmod.org>
2013-08-27 22:35:37 +0200

    Revert "Bluetooth : hidp: Get the valid hci_conn while unlink"
    This reverts commit 686ac733f0901b66e6d29c5eb7666ae48cca0143.

--- net/bluetooth/hidp/core.c
+++ net/bluetooth/hidp/core.c
@@ -101,20 +101,7 @@ static void __hidp_link_session(struct hidp_session *session)
 
 static void __hidp_unlink_session(struct hidp_session *session)
 {
-	bdaddr_t *dst = &session->bdaddr;
-	struct hci_dev *hdev;
-	struct device *dev = NULL;
-
-	hdev = hci_get_route(dst, BDADDR_ANY);
-	if (hdev) {
-		session->conn = hci_conn_hash_lookup_ba(hdev, ACL_LINK, dst);
-		if (session->conn)
-			dev = &session->conn->dev;
-
-		hci_dev_put(hdev);
-	}
-
-	if (dev)
+	if (session->conn)
 		hci_conn_put_device(session->conn);
 
 	list_del(&session->list);
