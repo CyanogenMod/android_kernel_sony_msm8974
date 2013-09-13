ca1a993e8b9f4042d2fc372d8cbd55172da25ca4
Naseer Ahmed <naseer@codeaurora.org>
2013-09-13 15:41:37 -0400

    mdss: Increase display commit thread priority
    The display commit thread queues up the frames being transferred.
    Increase priority to reduce jank.
    
    Signed-off-by: Naseer Ahmed <naseer@codeaurora.org>
    
    Conflicts:
    	drivers/video/msm/mdss/mdss_fb.c
    
    Change-Id: I5dfdaf383e9c3d356fee801bda2750773a2a0454

--- drivers/video/msm/mdss/mdss_fb.c
+++ drivers/video/msm/mdss/mdss_fb.c
@@ -1831,12 +1831,10 @@ static int __mdss_fb_display_thread(void *data)
 	struct msm_fb_data_type *mfd = data;
 	int ret;
 	struct sched_param param;
-
 	param.sched_priority = 16;
 	ret = sched_setscheduler(current, SCHED_FIFO, &param);
 	if (ret)
-		pr_warn("set priority failed for fb%d display thread\n",
-				mfd->index);
+		pr_info("%s: set priority failed\n", __func__);
 
 	while (1) {
 		wait_event_interruptible(mfd->commit_wait_q,
