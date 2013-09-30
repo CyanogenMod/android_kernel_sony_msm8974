b4a2a32fe16be64d23cb04ddea7b560b3c8ce4ee
Adrian Salido-Moreno <adrianm@codeaurora.org>
2013-09-30 15:35:42 -0700

    msm: mdss: fix sw calculation of interlace x offset
    Interlacing is handled in software by changing the original height to
    half which would become the width after rotation. In cases of 90 degree
    rotation the height becomes the displayed width so in that case the
    source x offset needs to be updated to half of intended similar to
    source width.
    
    CRs-Fixed: 545926
    Change-Id: I0de9adb243d6162f5387b1ebc2821141eea4b111
    Signed-off-by: Adrian Salido-Moreno <adrianm@codeaurora.org>
    Signed-off-by: Pawan Kumar <pavaku@codeaurora.org>

--- drivers/video/msm/mdss/mdss_mdp_overlay.c
+++ drivers/video/msm/mdss/mdss_mdp_overlay.c
@@ -562,6 +562,7 @@ static int mdss_mdp_overlay_pipe_setup(struct msm_fb_data_type *mfd,
 
 	if ((pipe->flags & MDP_DEINTERLACE) && !pipe->scale.enable_pxl_ext) {
 		if (pipe->flags & MDP_SOURCE_ROTATED_90) {
+			pipe->src.x = DIV_ROUND_UP(pipe->src.x, 2);
 			pipe->src.w /= 2;
 			pipe->img_width /= 2;
 		} else {
