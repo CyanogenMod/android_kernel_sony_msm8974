662d9d688e869088a7c785efa663b09732bc033c
Carl Vanderlip <carlv@codeaurora.org>
2013-10-30 19:09:14 -0700

    msm: mdss: Control histogram intr and clk in suspend cases
    Histogram interrupts, and clocks used to handle interrupts, need to be
    enabled in sync with histogram requests. To ensure that these interrupts
    and clocks are disabled when screen is suspended or not updating, a control
    structure is introduced that allows for these resources to be gated by
    specifying transitions from when they should and should not be active.
    
    CRs-Fixed: 568049
    Change-Id: I77a50085ee38de597092c273c7ce9974c7bb29c3
    Signed-off-by: Carl Vanderlip <carlv@codeaurora.org>
    
    Conflicts:
    	drivers/video/msm/mdss/mdss.h

--- drivers/video/msm/mdss/mdss_mdp_ctl.c
+++ drivers/video/msm/mdss/mdss_mdp_ctl.c
@@ -1119,6 +1119,7 @@ static int mdss_mdp_ctl_start_sub(struct mdss_mdp_ctl *ctl, bool handoff)
 int mdss_mdp_ctl_start(struct mdss_mdp_ctl *ctl, bool handoff)
 {
 	struct mdss_mdp_ctl *sctl;
+	struct mdss_data_type *mdata = mdss_mdp_get_mdata();
 	int ret = 0;
 
 	if (ctl->power_on) {
@@ -1171,6 +1172,7 @@ int mdss_mdp_ctl_start(struct mdss_mdp_ctl *ctl, bool handoff)
 			mdss_mdp_ctl_write(ctl, MDSS_MDP_REG_CTL_PACK_3D, 0);
 		}
 	}
+	mdss_mdp_hist_intr_setup(&mdata->hist_intr, MDSS_IRQ_RESUME);
 
 	mdss_mdp_clk_ctrl(MDP_BLOCK_POWER_OFF, false);
 error:
@@ -1183,6 +1185,7 @@ int mdss_mdp_ctl_stop(struct mdss_mdp_ctl *ctl)
 {
 	struct mdss_mdp_ctl *sctl;
 	int ret = 0;
+	struct mdss_data_type *mdata = mdss_mdp_get_mdata();
 	u32 off;
 
 	if (!ctl->power_on) {
@@ -1197,6 +1200,8 @@ int mdss_mdp_ctl_stop(struct mdss_mdp_ctl *ctl)
 	mutex_lock(&ctl->lock);
 
 	mdss_mdp_clk_ctrl(MDP_BLOCK_POWER_ON, false);
+
+	mdss_mdp_hist_intr_setup(&mdata->hist_intr, MDSS_IRQ_SUSPEND);
 
 	if (ctl->stop_fnc)
 		ret = ctl->stop_fnc(ctl);
