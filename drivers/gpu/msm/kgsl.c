b005bbf0e2078483ea4eb58d3157893ac9fe3b9b
Jeremy Gebben <jgebben@codeaurora.org>
2013-11-05 10:01:15 -0700

    msm: kgsl: manage active count in adreno_ioctl()
    Most of the adreno specific ioctls do not need the hardware
    on. The few that do are perfcounter related, it may be
    better at some point to optimize them further to shadow
    the counters when the hardware is not on rather than
    forcing the power on.
    
    Change-Id: I9ea83106e0aaf3d3ac8d261bdec4e4c2e21c4659
    Signed-off-by: Jeremy Gebben <jgebben@codeaurora.org>

--- drivers/gpu/msm/kgsl.c
+++ drivers/gpu/msm/kgsl.c
@@ -3382,7 +3382,7 @@ static long kgsl_ioctl(struct file *filep, unsigned int cmd, unsigned long arg)
 	struct kgsl_device_private *dev_priv = filep->private_data;
 	unsigned int nr;
 	kgsl_ioctl_func_t func;
-	int lock, ret, use_hw;
+	int lock, ret, use_hw = 0;
 	char ustack[64];
 	void *uptr = NULL;
 
@@ -3450,7 +3450,6 @@ static long kgsl_ioctl(struct file *filep, unsigned int cmd, unsigned long arg)
 			goto done;
 		}
 		lock = 1;
-		use_hw = 1;
 	}
 
 	if (lock) {
