fbdd57e6a733b83c741c00b022ff2779ef869a11
Deva Ramasubramanian <dramasub@codeaurora.org>
2014-02-18 17:39:29 -0800

    msm: vidc: Free smem client after moving core to UNINIT
    If userspace crashes prematurely, there will be a good chance that the
    driver hasn't unmapped or freed certain buffers.  These buffers will be
    freed in the state transition to UNINIT.  At that point the smem client
    needs to be valid for the free/unmaps to be valid.
    
    Change-Id: I089dcbd1e79f1c2a8671e5719d3a5dbc9926b252
    Signed-off-by: Deva Ramasubramanian <dramasub@codeaurora.org>

--- drivers/media/platform/msm/vidc/msm_vidc.c
+++ drivers/media/platform/msm/vidc/msm_vidc.c
@@ -1334,7 +1334,6 @@ static void cleanup_instance(struct msm_vidc_inst *inst)
 			mutex_lock(&inst->lock);
 		}
 		mutex_unlock(&inst->lock);
-		msm_smem_delete_client(inst->mem_client);
 		debugfs_remove_recursive(inst->debugfs_root);
 	}
 }
@@ -1392,7 +1391,10 @@ int msm_vidc_close(void *instance)
 	for (i = 0; i < MAX_PORT_NUM; i++)
 		vb2_queue_release(&inst->bufq[i].vb2_bufq);
 
+	msm_smem_delete_client(inst->mem_client);
+
 	pr_info(VIDC_DBG_TAG "Closed video instance: %p\n", VIDC_INFO, inst);
 	kfree(inst);
+
 	return 0;
 }
