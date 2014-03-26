30c09516752259c616525a803c4bf9ac1e72b42c
Tarun Karra <tkarra@codeaurora.org>
2014-03-25 18:13:53 -0700

    msm: kgsl: Skip the entire command during fault recovery
    During fault recovery the algorithm may choose to skip the faulting
    command. To skip just one indirect buffer we have to rely on
    CP_IB1_BASE but depending on where the fault happened the base could
    already be pointing to the next buffer. This causes the code to skip
    over the wrong buffer. There is no advantage to skipping just one
    buffer vs the entire submission.
    
    Conflicts:
            drivers/gpu/msm/adreno.h
            drivers/gpu/msm/adreno_drawctxt.h
    
    Change-Id: I28dc999b2156d05f7b5084df6bdd95c5b9308579
    Signed-off-by: Tarun Karra <tkarra@codeaurora.org>
    Signed-off-by: asmathur <asmathur@codeaurora.org>

--- drivers/gpu/msm/adreno.h
+++ drivers/gpu/msm/adreno.h
@@ -418,6 +418,7 @@ struct log_field {
 #define  KGSL_FT_DISABLE                  4
 #define  KGSL_FT_TEMP_DISABLE             5
 #define  KGSL_FT_DEFAULT_POLICY (BIT(KGSL_FT_REPLAY) + BIT(KGSL_FT_SKIPIB))
+#define  KGSL_FT_SKIPCMD                  6
 
 /* This internal bit is used to skip the PM dump on replayed command batches */
 #define  KGSL_FT_SKIP_PMDUMP              31
@@ -435,7 +436,8 @@ struct log_field {
 	{ BIT(KGSL_FT_SKIPIB), "skipib" }, \
 	{ BIT(KGSL_FT_SKIPFRAME), "skipframe" }, \
 	{ BIT(KGSL_FT_DISABLE), "disable" }, \
-	{ BIT(KGSL_FT_TEMP_DISABLE), "temp" }
+	{ BIT(KGSL_FT_TEMP_DISABLE), "temp" }, \
+	{ BIT(KGSL_FT_SKIPCMD), "skipcmd" }
 
 extern struct adreno_gpudev adreno_a2xx_gpudev;
 extern struct adreno_gpudev adreno_a3xx_gpudev;
@@ -514,6 +516,10 @@ int adreno_reset(struct kgsl_device *device);
 
 int adreno_ft_init_sysfs(struct kgsl_device *device);
 void adreno_ft_uninit_sysfs(struct kgsl_device *device);
+
+void adreno_fault_skipcmd_detached(struct kgsl_device *device,
+					 struct adreno_context *drawctxt,
+					 struct kgsl_cmdbatch *cmdbatch);
 
 int adreno_perfcounter_get_groupid(struct adreno_device *adreno_dev,
 					const char *name);
