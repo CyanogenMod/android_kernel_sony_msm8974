41972521ff26efa54c01ccd9aaa3c277527e667a
Sujit Reddy Thumma <sthumma@codeaurora.org>
2014-06-10 17:57:52 +0530

    mmc: core: Fix null pointer dereference due to race conditions
    Fix race condition between mmcqd thread and the mmc_queue_suspend
    updating a shared variable mq->flags, which can lead to potential
    null pointer dereference as following-
    
    Unable to handle kernel NULL pointer dereference at
    virtual address 00000020
    pgd = c0004000
    [00000020] *pgd=00000000
    mmcqd/0:  186] Internal error: Oops: 5 [#1] PREEMPT SMP ARM
    CPU: 0    Tainted: G        W     (3.4.0-1251694-eng #1)
    PC is at mmc_blk_err_check+0x20c/0x3b8
    LR is at mmc_start_req+0x198/0x718
    
    cpu0		|	cpu1
    x |= 1		|	x |= 2
    
    final value of x can be x = 1 or x = 2
    
    Change-Id: Ie0fff6d6dba5aebb3584cba9fb98de24515c4cd8
    Signed-off-by: Sujit Reddy Thumma <sthumma@codeaurora.org>
    
    Conflicts:
    	drivers/mmc/card/block.c
    	drivers/mmc/card/queue.c

--- drivers/mmc/card/block.c
+++ drivers/mmc/card/block.c
@@ -31,6 +31,7 @@
 #include <linux/blkdev.h>
 #include <linux/mutex.h>
 #include <linux/scatterlist.h>
+#include <linux/bitops.h>
 #include <linux/string_helpers.h>
 #include <linux/delay.h>
 #include <linux/capability.h>
@@ -2503,7 +2504,7 @@ static int mmc_blk_issue_rw_rq(struct mmc_queue *mq, struct request *rqc)
 		areq = mmc_start_req(card->host, areq, (int *) &status);
 		if (!areq) {
 			if (status == MMC_BLK_NEW_REQUEST)
-				mq->flags |= MMC_QUEUE_NEW_REQUEST;
+				set_bit(MMC_QUEUE_NEW_REQUEST, &mq->flags);
 			return 0;
 		}
 
@@ -2524,7 +2525,7 @@ static int mmc_blk_issue_rw_rq(struct mmc_queue *mq, struct request *rqc)
 				mmc_blk_reinsert_req(areq);
 			}
 
-			mq->flags |= MMC_QUEUE_URGENT_REQUEST;
+			set_bit(MMC_QUEUE_URGENT_REQUEST, &mq->flags);
 			ret = 0;
 			break;
 		case MMC_BLK_URGENT_DONE:
@@ -2710,9 +2711,9 @@ static int mmc_blk_issue_rq(struct mmc_queue *mq, struct request *req)
 
 	mmc_blk_write_packing_control(mq, req);
 
-	mq->flags &= ~MMC_QUEUE_NEW_REQUEST;
-	mq->flags &= ~MMC_QUEUE_URGENT_REQUEST;
-	if (cmd_flags & REQ_SANITIZE) {
+	clear_bit(MMC_QUEUE_NEW_REQUEST, &mq->flags);
+	clear_bit(MMC_QUEUE_URGENT_REQUEST, &mq->flags);
+	if (req && req->cmd_flags & REQ_SANITIZE) {
 		/* complete ongoing async transfer before issuing sanitize */
 		if (card->host && card->host->areq)
 			mmc_blk_issue_rw_rq(mq, NULL);
@@ -2747,9 +2748,10 @@ out:
 	 * - urgent notification in progress and current request is not urgent
 	 *   (all existing requests completed or reinserted to the block layer)
 	 */
-	if ((!req && !(mq->flags & MMC_QUEUE_NEW_REQUEST)) ||
-			((mq->flags & MMC_QUEUE_URGENT_REQUEST) &&
-			 !(cmd_flags & MMC_REQ_NOREINSERT_MASK))) {
+	if ((!req && !(test_bit(MMC_QUEUE_NEW_REQUEST, &mq->flags))) ||
+			((test_bit(MMC_QUEUE_URGENT_REQUEST, &mq->flags)) &&
+			 !(mq->mqrq_cur->req->cmd_flags &
+				MMC_REQ_NOREINSERT_MASK))) {
 		if (mmc_card_need_bkops(card))
 			mmc_start_bkops(card, false);
 		/* release host only when there are no more requests */
