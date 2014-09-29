bae9102320548a63bd1b354a59ce40585c4a7480
Giulio Cervera <giulio.cervera@cyanogenmod.org>
2014-09-30 01:42:01 +0200

    driver: kgsl: fix merge
    Change-Id: I0663136ff7da27ec188b8c4d25cc466eccb26d6a

--- drivers/gpu/msm/adreno.h
+++ drivers/gpu/msm/adreno.h
@@ -906,7 +906,6 @@ adreno_get_rptr(struct adreno_ringbuffer *rb)
 	adreno_readreg(adreno_dev, ADRENO_REG_CP_RB_RPTR, &result);
 	return result;
 }
-
 /*
  * adreno_set_protected_registers() - Protect the specified range of registers
  * from being accessed by the GPU
@@ -944,11 +943,5 @@ static inline void adreno_set_protected_registers(struct kgsl_device *device,
 	kgsl_regwrite(device, A3XX_CP_PROTECT_REG_0 + *index, val);
 	*index = *index + 1;
 }
-
-#ifdef CONFIG_DEBUG_FS
-void adreno_debugfs_init(struct kgsl_device *device);
-#else
-static inline void adreno_debugfs_init(struct kgsl_device *device) { }
-#endif
 
 #endif /*__ADRENO_H */
--- drivers/gpu/msm/adreno_a3xx.c
+++ drivers/gpu/msm/adreno_a3xx.c
@@ -3120,7 +3120,8 @@ static void a3xx_err_callback(struct adreno_device *adreno_dev, int bit)
 			"CP | Protected mode error| %s | addr=%x\n",
 			reg & (1 << 24) ? "WRITE" : "READ",
 			(reg & 0x1FFFF) >> 2);
-		break;
+
+		return;
 	}
 	case A3XX_INT_CP_AHB_ERROR_HALT:
 		err = "ringbuffer AHB error interrupt";
